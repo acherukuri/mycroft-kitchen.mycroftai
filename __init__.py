562128846d1887c1676a113d159da8b5331395bd
Anil Cherukuri <anil.cherukuri03@gmail.com>
2019-04-07 11:38:03 -0500

    fetches the ingredients for dish

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import requests
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 from mycroft import intent_file_handler
@@ -7,12 +8,22 @@ LOGGER = getLogger(__name__)
 class KitchenSkill(MycroftSkill):
     def __init__(self):
         super(KitchenSkill, self).__init__(name="KitchenSkill")
+        self.recipe_search_url = "https://api.edamam.com/search?q={}&app_id=45d96e9b&app_key=340e6e97add2df98e6d6dd76c52efa19&from=0&to=1"
     
     def get_recipe_for_dish(self, message):
         dish = message.data.get('dish')
-        data = {"dish": dish}
-        self.log.info('dish name is: ' + dish)
-        self.speak_dialog("recipe", data)
+        lookup_dish = {"dish": dish}
+        self.speak_dialog("looking.up.now", lookup_dish)
+        resp = requests.get(self.recipe_search_url.format(dish))
+        if resp.status_code != 200:
+            self.speak_dialog("error.occurred")
+            return None
+        elif resp.status_code == 200:
+            resp = resp.json()
+            recipe = resp['hits'][0]['recipe'] # TODO: handle empty recipe response
+            data = {"dish": dish, "source": recipe['source'],
+                    "ingredients": ', '.join(recipe['ingredientLines'])}
+            self.speak_dialog("recipe", data)
 
     @intent_file_handler("recipe.ingredients.intent")
     def handle_recipe_ingredients_intent(self, message):
@@ -20,7 +31,6 @@ class KitchenSkill(MycroftSkill):
 
     def stop(self):
         pass
-
 
 def create_skill():
     return KitchenSkill()
